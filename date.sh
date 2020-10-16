4b08c11de1cb59b984d3a30626f3d2cb65846312
Taiki Sugawara <buzz.taiki@gmail.com>
2020-10-17 06:52:48 +0900

    eeek

---
+++ date.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -x
+
+which gdate
+perl --version
+echo "2020-01-02T03:04:05Z" | perl -mTime::Piece -nlE 'say Time::Piece->strptime($_, "%FT%TZ")'
+echo "2020-01-02T03:04:05+0900" | perl -mTime::Piece -nlE 'say Time::Piece->strptime($_, "%FT%T%z")'
